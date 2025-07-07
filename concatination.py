65b75b84a274fe9add5ee1d6e82a0664274f92af
Prakhyath Shri <prakhyath2004@gmail.com>
2025-07-07 19:51:52 +0530

    Concatination file

---
+++ concatination.py
@@ -0,0 +1,10 @@
+# String and Numeric values can operate together with *  
+a , b = 2, 3
+txt = "@ "
+print (a*txt*b)
+
+
+# String and String  can operate together with +   
+x , y = "2 ", 3
+text = "# "
+print ((x+text)*y) 
