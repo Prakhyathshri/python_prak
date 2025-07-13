21b71fc2e9a4080ee2977de4008036d46bc24773
Prakhyath Shri <prakhyath2004@gmail.com>
2025-07-13 10:07:23 +0530

    For loops

---
+++ for_p1.py
@@ -0,0 +1,15 @@
+list1 = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100,49)
+
+# for i in list1:
+#     print(i)
+
+x = int(input("Enter the searching number: "))
+for idx, value in enumerate(list1):
+    if (x == value):
+        print("x found at ", idx)
+        # break
+    print("finding.... at index ",idx)
+else:
+    print("Not Found")
+
+# we can use break if we want the number to searched only once 
